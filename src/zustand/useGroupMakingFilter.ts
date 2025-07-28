a2003313a863c934cf1a01617b38595b077d0f1b
JeonSuna <jsa020910@gmail.com>
2025-07-28 13:52:20 +0900

    feat: groupMaking store생성

---
+++ src/zustand/useGroupMakingFilter.ts
@@ -0,0 +1,44 @@
+import { create } from "zustand";
+
+export type FilterKey =
+  | "region"
+  | "FemaleLevel"
+  | "maleLevel"
+  | "style"
+  | "time";
+
+export interface ExerciseFilterState {
+  region: string[];
+  FemaleLevel: string[];
+  maleLevel: string[];
+  style: string;
+  time: string;
+  setFilter: (key: FilterKey, value: string[] | string) => void;
+  resetFilter: () => void;
+}
+
+export const useGroupMakingFilterStore = create<ExerciseFilterState>(set => ({
+  region: [],
+  FemaleLevel: [],
+  maleLevel: [],
+  style: "",
+  time: "",
+  setFilter: (key, value) => set(state => ({ ...state, [key]: value })),
+  resetFilter: () =>
+    set(() => ({
+      region: [],
+      level: [],
+      style: "",
+      time: "",
+    })),
+}));
+
+export const isFilterDirty = (filter: Pick<ExerciseFilterState, FilterKey>) => {
+  return (
+    filter.region.length > 0 ||
+    filter.FemaleLevel.length > 0 ||
+    filter.maleLevel.length > 0 ||
+    filter.style !== "" ||
+    filter.time !== ""
+  );
+};
