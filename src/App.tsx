e75fd4c6c3a4b6cba510bad4e9c90226e8fc9e4e
김태연 <te4939@naver.com>
2025-07-31 00:54:09 +0900

    오류 확인 완료

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ import {
   MyPage,
   MyPageEditLocationPage,
   MyPageEditPage,
-  MyPageExerciseDetailPage,
+  // MyPageExerciseDetailPage,
   MyPageMedalAddPage,
   MyPageMedalDetailPage,
   MyPageMyExercisePage,
