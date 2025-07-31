898351b1e81d90c04f515dde61f8009a7547b35d
qowldud <bjy6631@daum.net>
2025-08-01 01:09:03 +0900

    feat: MyPageMyExercisePage.tsx에 TabSelector로 변경

--- src/components/common/TabSelector.tsx
+++ src/components/common/TabSelector.tsx
@@ -20,7 +20,7 @@ const TabSelector = <T extends string>({
   onChange,
 }: TabSelectorProps<T>) => {
   return (
-    <div className="w-full -ml-4 px-4 max-w-[444px] flex flex-col mb-4 fixed top-14 z-50 bg-white">
+    <div className="w-full -ml-4 px-4 max-w-[444px] flex flex-col mb-4 fixed top-14 z-20 bg-white">
       <div className="flex text-black items-center gap-x-3">
         {options.map(option => (
           <TabBtn
