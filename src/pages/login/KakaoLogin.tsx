18bdf37d904b3edf8c74fb52c524ab817199635d
JeonSuna <jsa020910@gmail.com>
2025-08-05 15:53:16 +0900

    refactor: react-query로 변경

--- src/pages/login/KakaoLogin.tsx
+++ src/pages/login/KakaoLogin.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import useUserStore from "../../store/useUserStore";
+import { useMutation } from "@tanstack/react-query";
+import api from "../../api/api";
+
 export default function KakaoLogin() {
   const navigate = useNavigate();
   const { setUser } = useUserStore();
@@ -11,60 +13,35 @@ export default function KakaoLogin() {
     const code = params.get("code");
 
     if (code) {
-      sendKakaoRequest(code);
+      sendKakaoRequest.mutate({ code });
     }
   }, [location.search]);
 
-  const sendKakaoRequest = async (code: string) => {
-    try {
-      const response = await axios.post(
-        `https://cockple.store/api/oauth/login`,
-        { code },
-        { withCredentials: true },
-      );
-
-      const { data } = response;
+  const axios = api;
+  const sendKakaoRequest = useMutation({
+    mutationFn: ({ code }: { code: string }) => {
+      return axios.post("/api/oauth/login", { code });
+    },
+    onSuccess: data => {
+      console.log("성공");
       console.log(data);
       const newUserData = {
-        memberId: data.mamberId,
-        nickname: data.nickname,
-        accessToken: data.accessToken,
-        refreshToken: data.refreshToken,
+        memberId: data.data.mamberId,
+        nickname: data.data.nickname,
+        accessToken: data.data.accessToken,
+        refreshToken: data.data.refreshToken,
       };
       setUser(newUserData);
-      if (data.isNewMember) {
+      if (data.data.isNewMember) {
         //미사용자
         navigate("/onboarding");
       } else {
         //기존 사용자
         navigate("/");
       }
-    } catch (error) {
+    },
+    onError: error => {
       console.log(error);
-    }
-  };
+    },
+  });
 }
-
-// const axios = api;
-
-//   useEffect(() => {
-//     const params = new URLSearchParams(location.search);
-//     const code = params.get("code");
-
-//     if (code) {
-//       sendKakao.mutate({ code });
-//     }
-//   }, [location.search]);
-
-//   const sendKakao = useMutation({
-//     mutationFn: ({ code }: { code: string }) => {
-//       return axios.post("/api/oauth/login", { code });
-//     },
-//     onSuccess: data => {
-//       console.log("성공");
-//       console.log(data);
-//     },
-//     onError: error => {
-//       console.log(error);
-//     },
-//   });
