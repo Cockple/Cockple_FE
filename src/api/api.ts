faf1e683d9dcd0db59629c4ac9d47d387a073e1c
JeonSuna <jsa020910@gmail.com>
2025-08-07 20:37:50 +0900

    feat:.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-const raw = JSON.parse(sessionStorage.getItem("user"));
-const accessToken = raw.state.user.accessToken;
-console.log(accessToken);
 const api = axios.create({
   baseURL: "https://cockple.store",
-  headers: {
-    Authorization: `Bearer ${accessToken}`, // ✅ 이게 있어야 함
-  },
 });
 
 export default api;
