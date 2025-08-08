27b293fae24cac7ceb96d33f2d8cf3c3e56a197b
qowldud <bjy6631@daum.net>
2025-08-09 03:01:57 +0900

    feat: 사용안하는 변수 제거

--- src/pages/group/GroupHomePage.tsx
+++ src/pages/group/GroupHomePage.tsx
@@ -15,23 +15,6 @@ import { useNavigate, useParams } from "react-router-dom";
 import Grad_Mix_L from "../../components/common/Btn_Static/Text/Grad_Mix_L";
 import { usePartyDetail } from "../../api/exercise/getpartyDetail";
 
-const dummyClub = {
-  id: "mint-clover",
-  name: "민트클로버",
-  region: "경기도 / 성남시",
-  schedule: "월 화 수 목 금 토",
-  time: "상시",
-  level: ["전국 초심 ~ 준자강", "전국 준자강 이상"],
-  ageRange: "1990 ~ 2005년생",
-  fee: "10,000원",
-  joinFee: "10,000원",
-  courts: ["삼화다목적", "삼화다목적", "삼화다목적"],
-  tags: ["브랜드 스폰", "가입비 무료", "친목", "운동"],
-  image: "/images/sample_club.png", // ← 실제 이미지 경로로 바꿔줘!
-  description:
-    "민트클로버는 성남시 2030을 대상으로 하는 운동입니다! 함께 하고싶으신 분들은 연락주세요~",
-};
-
 export const GroupHomePage = () => {
   const [isExpanded, setIsExpanded] = useState(false);
   const [rightOffset, setRightOffset] = useState(0);
