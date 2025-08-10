ea59f73fda22e3ab5de801fa244c4de3f372d9b9
김태연 <te4939@naver.com>
2025-08-10 21:37:00 +0900

    충돌 발생 해결

--- src/api/api.ts
+++ src/api/api.ts
@@ -18,12 +18,3 @@ api.interceptors.request.use(
     return Promise.reject(error);
   },
 );
-
-export default api;
-const token = import.meta.env.VITE_APP_ACCESS_TOKEN;
-api.interceptors.request.use((config) => {
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
