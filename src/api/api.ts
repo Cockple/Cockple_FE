2dbd89727c82eaad7a066ce435ce5e0174a0fba9
김태연 <te4939@naver.com>
2025-08-10 21:36:38 +0900

    충돌 발생

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,3 +4,11 @@ const api = axios.create({
   baseURL: "https://cockple.store",
 });
 
+export default api;
+const token = import.meta.env.VITE_APP_ACCESS_TOKEN;
+api.interceptors.request.use((config) => {
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
