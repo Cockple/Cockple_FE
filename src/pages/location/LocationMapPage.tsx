a7e5152e156005eb7bab40a480ef0e1a54ea3082
qowldud <bjy6631@daum.net>
2025-08-21 02:07:35 +0900

    fix: 맵에서 운동선택 오류 수정

--- src/pages/location/LocationMapPage.tsx
+++ src/pages/location/LocationMapPage.tsx
@@ -42,7 +42,7 @@ export const LocationMapPage = () => {
       navigate(returnPath);
     } else {
       navigate(returnPath, {
-        state: { payload },
+        state: { selectedPlace: place },
       });
       console.log(returnPath);
     }
