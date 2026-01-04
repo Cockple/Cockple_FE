7e397e993d7e2bea5ca2d450f00bcc7d505ef8f2
qowldud <bjy6631@daum.net>
2026-01-04 23:02:34 +0900

    fix: tsconfig.app에 @경로 alias 설정 추가

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
-import { MainHeader } from "../../components/common/system/header/MainHeader";
-import { Footer } from "../../components/common/system/Footer";
-import { MyGroupWorkoutSection } from "../../components/home/MyGroupWorkoutSection";
+import { MyGroupWorkoutSection } from "@/components/home/MyGroupWorkoutSection";
 import { RecommendedWorkoutSection } from "../../components/home/RecommendedWorkoutSection";
 import { FloatingButton } from "../../components/common/system/FloatingButton";
 import MapIcon from "@/assets/icons/map_white.svg";
 import { useNavigate } from "react-router-dom";
 import { MyExerciseCalendar } from "../../components/home/MyExerciseCalendar";
+import { MainHeader } from "../../components/common/system/header/MainHeader";
+import { Footer } from "../../components/common/system/Footer";
 
 export type DailyExerciseItem = {
   id: number;
