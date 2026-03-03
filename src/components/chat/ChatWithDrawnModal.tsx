3cf6890123d2736306073ac6d98dbfe8b5bcaa0f
JeonSuna <jsa020910@gmail.com>
2026-03-03 18:02:38 +0900

    design: 회원탈퇴 모달 x제거

--- src/components/chat/ChatWithDrawnModal.tsx
+++ src/components/chat/ChatWithDrawnModal.tsx
@@ -1,5 +1,4 @@
 import GY800_S from "../../components/common/Btn_Static/Text/GY800_S";
-import { ModalBar } from "../../components/common/system/ModalBar";
 import Emoji from "@/assets/icons/emoji_surprise.svg";
 
 interface ChatWithDrawnModalProps {
@@ -9,9 +8,9 @@ interface ChatWithDrawnModalProps {
 export const ChatWithDrawnModal = ({ onClose }: ChatWithDrawnModalProps) => {
   return (
     <div className="flex justify-center items-center fixed bottom-0 bg-black/20 -mx-4 w-full max-w-[444px] h-full z-50">
-      <div className="w-86 h-45 p-3 bg-white shadow-ds200 border-round">
-        <ModalBar onClick={onClose} />
-        <div className="flex flex-col gap-vertical-section-s">
+      <div className="w-86 h-45 p-3 bg-white shadow-ds200 border-round flex flex-col justify-end">
+        {/* <ModalBar onClick={onClose} /> */}
+        <div className="flex flex-col   gap-vertical-section-s">
           <div className="flex flex-col justify-center items-center gap-2">
             <img src={Emoji} alt="emogi_surprise" className="w-8 h-8" />
             <div className="header-h4 text-black">
