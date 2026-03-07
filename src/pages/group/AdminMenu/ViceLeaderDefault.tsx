2cb82644cbd46b8efcf7c886b8e925ea64b5786e
kimtaeyeon04 <tae49397691@gmail.com>
2026-03-07 23:24:45 +0900

    dev 충돌

--- src/pages/group/AdminMenu/ViceLeaderDefault.tsx
+++ src/pages/group/AdminMenu/ViceLeaderDefault.tsx
@@ -40,7 +40,14 @@ export const ViceLeaderDefault = () => {
   const mapApiMemberToMemberProps = (m: ApiMember): MemberProps => ({
     memberId: m.memberId,
     name: m.nickname,
-    imgUrl: m.profileImageUrl || null,
+    // imgUrl: m.profileImageUrl || null,
+
+    imgUrl: m.profileImageUrl
+      ? m.profileImageUrl.startsWith("http")
+        ? m.profileImageUrl 
+        : `https://storage.googleapis.com/cockple-assets-project-fcaa6e71-8bce-4fb7-9de/${m.profileImageUrl}` 
+      : null,
+      
     gender: m.gender,
     level: m.level,
     lastExerciseDate: m.lastExerciseDate,
--- src/pages/group/MyGroupDetail/MemberDefault.tsx
+++ src/pages/group/MyGroupDetail/MemberDefault.tsx
@@ -40,9 +40,12 @@ export const MemberDefault = () => {
     name: m.nickname,
     imgUrl: m.profileImageUrl || null,
     //임시로 수정
-    // imgUrl: m.profileImageUrl
-    //   ? `https://storage.googleapis.com/cockple-assets-project-fcaa6e71-8bce-4fb7-9de/${m.profileImageUrl}`
-    //   : null,
+    imgUrl: m.profileImageUrl
+      ? m.profileImageUrl.startsWith("http")
+        ? m.profileImageUrl 
+        : `https://storage.googleapis.com/cockple-assets-project-fcaa6e71-8bce-4fb7-9de/${m.profileImageUrl}` 
+      : null,
+
     gender: m.gender,
     level: m.level,
     lastExerciseDate: m.lastExerciseDate,
