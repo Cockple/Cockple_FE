2d8c1abc186343fec9d1a5b28d96dcee3898e1c8
qowldud <bjy6631@daum.net>
2026-03-21 01:28:28 +0900

    feat: fcm 푸시 알림 세팅

---
+++ src/hooks/useFcmToken.ts
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { messaging, onMessage, requestFcmToken } from "../firebase";
+
+export function useFcmToken() {
+  useEffect(() => {
+    requestFcmToken().then((token) => {
+      if (!token) return;
+      console.log("FCM Token:", token);
+      // TODO: 백엔드 토큰 전송 API 연동
+      // api.post("/notifications/token", { token });
+    });
+
+    // 포그라운드(앱이 열려있을 때) 알림 수신
+    const unsubscribe = onMessage(messaging, (payload) => {
+      console.log("포그라운드 알림 수신:", payload);
+      // TODO: 토스트 등 UI 알림 표시
+    });
+
+    return () => unsubscribe();
+  }, []);
+}
