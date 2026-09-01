52977cb3fbb403d22ede13624e5e992c3ab840c4
qowldud <bjy6631@daum.net>
2026-09-01 23:57:08 +0900

    fix: 모임 캘린더 운동 없는 날 공유 버튼 숨김 및 마지막 카드 하단 구분선 제거

--- src/pages/group/GroupCalendarPage.tsx
+++ src/pages/group/GroupCalendarPage.tsx
@@ -65,9 +65,13 @@ export const GroupCalendarPage = () => {
 
       <div className="flex flex-col pb-15">
         {selectedDayExercises.length > 0 ? (
-          selectedDayExercises.map(exercise => (
+          selectedDayExercises.map((exercise, index) => (
             <div
-              className="border-b-1 border-gy-200 mb-3"
+              className={`mb-3 ${
+                index < selectedDayExercises.length - 1
+                  ? "border-b-1 border-gy-200"
+                  : ""
+              }`}
               key={exercise.exerciseId}
             >
               <ContentCardL
